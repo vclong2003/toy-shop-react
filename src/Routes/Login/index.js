ceeefb33938188339c12aea19993c4e7f42a0fff
vclong2003 <vclong2003@gmail.com>
2023-02-11 18:47:49 +0700

    backUp

--- src/Routes/Login/index.js
+++ src/Routes/Login/index.js
@@ -1,26 +1,58 @@
+import axios from "axios";
+import React from "react";
 import { Button, Container, Form } from "react-bootstrap";
+import { api_endpoint } from "../../config";
 
 export default function Login() {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleLogin = (evt) => {
+    evt.preventDefault();
+
+    axios
+      .post(
+        `${api_endpoint}/auth/login`,
+        {
+          email: email,
+          password: password,
+        },
+        { withCredentials: true }
+      )
+      .then((response) => {
+        console.log(response.status);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <Container>
-      <Form>
+      <Form onSubmit={handleLogin}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(evt) => {
+              setEmail(evt.target.value);
+            }}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(evt) => {
+              setPassword(evt.target.value);
+            }}
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
-          Submit
+          Next
         </Button>
       </Form>
     </Container>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import axios from "axios";
+
 import Home from "./Routes/Home";
 import NavigationBar from "./Component/NavBar";
 import Login from "./Routes/Login";
 import Register from "./Routes/Register";
+import { api_endpoint } from "./config";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -14,13 +17,33 @@ root.render(
     <BrowserRouter>
       <NavigationBar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
+
+async function PrivateRoute({ children }) {
+  let user;
+  await axios
+    .get(`${api_endpoint}/user`)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  return children;
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
