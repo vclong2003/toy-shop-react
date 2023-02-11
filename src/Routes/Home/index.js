19d00ad17620cd8b1efb1278c528b406b58cd2d3
vclong2003 <vclong2003@gmail.com>
2023-02-11 17:56:11 +0700

    ok

--- src/Routes/Home/index.js
+++ src/Routes/Home/index.js
@@ -1,9 +1,26 @@
 import { Container } from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "../../counterSlice";
 
 export default function Home() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <Container>
-      <div>ok</div>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}>
+        +
+      </button>
+      <div>{count}</div>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}>
+        -
+      </button>
     </Container>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,16 +3,22 @@ import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Routes/Home";
+import NavigationBar from "./Component/NavBar";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
