90ad95538bfa6d1bb82d138b2748f49765a0130b
vclong2003 <vclong2003@gmail.com>
2023-03-01 21:44:41 +0700

    backUp

--- src/Redux/cart.js
+++ src/Redux/cart.js
@@ -4,10 +4,12 @@ export const cart = createSlice({
   name: "cart",
   initialState: {
     items: [],
+    count: 0,
   },
   reducers: {
     setCartItems: (state, action) => {
       state.items = action.payload;
+      state.count = state.items.length;
     },
   },
 });
